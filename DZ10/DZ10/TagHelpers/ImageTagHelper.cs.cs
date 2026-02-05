fa50b32405320a8788c96d8c9a800da557312905
Pascal100500 <kingandbuffon@gmail.com>
2026-02-05 22:57:32 +0300

    Add DZ10

---
+++ DZ10/DZ10/TagHelpers/ImageTagHelper.cs.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DZ10.TagHelpers
+{
+    public class ImageTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var imageName = output.GetChildContentAsync().Result.GetContent();
+            var imagePath = $"/images/{imageName}.jpg";
+            var physicalPath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "images",
+                $"{imageName}.jpg"
+            );
+
+            if (!File.Exists(physicalPath))
+            {
+                // При отсутствии картинки выведется текст с предупреждением
+                output.TagName = "span";
+                output.Content.SetContent("Картинка не загрузилась");
+                return;
+            }
+
+            output.TagName = "img";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.SetAttribute("src", imagePath);
+            output.Attributes.SetAttribute("alt", imageName);
+        }
+    }
+}
---
+++ DZ10/DZ10/TagHelpers/TimeTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DZ10.TagHelpers
+{
+    public class TimeTagHelper : TagHelper
+    {
+        // offset от GMT
+        public int Offset { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+
+            var time = DateTime.UtcNow.AddHours(Offset)
+                                      .ToString("HH:mm:ss");
+
+            output.Content.SetContent(time);
+        }
+    }
+}
